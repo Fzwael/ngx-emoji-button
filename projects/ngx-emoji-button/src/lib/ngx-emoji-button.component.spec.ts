687abdc2b24b335cac8559beec43860d62dd401c
Wael Fezai <fzwael@gmail.com>
2020-03-17 18:08:45 +0100

    Corrected eslint errors :recycle:

--- projects/ngx-emoji-button/src/lib/ngx-emoji-button.component.spec.ts
+++ projects/ngx-emoji-button/src/lib/ngx-emoji-button.component.spec.ts
@@ -10,7 +10,7 @@ describe('NgxEmojiButtonComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ NgxEmojiButtonComponent ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
